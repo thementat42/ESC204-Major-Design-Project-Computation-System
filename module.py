6c6115542c5f8812f2247a170620060f4ea3b72e
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-03-10 17:10:57 -0400

    add gps v1

--- module.py
+++ module.py
@@ -6,18 +6,18 @@ import adafruit_bme680
 import json
 import time
 import analogio
+import busio
+import adafruit_gps
 
 # Connect Pico to wi-fi (mine for now while testing)
 WIFI_SSID = "Reef"
 WIFI_PASSWORD = "Alexandria"
 MODULE_ID = "m01" # Make sure to change this for every pico
 LAPTOP_IP = "192.168.2.45" # Also change this depending on where and what we test this on
+ADC_HIGH = 65535
 
 wifi.radio.connect(WIFI_SSID, WIFI_PASSWORD)
 print("Successfully Connected. IP:", wifi.radio.ipv4_address)
-
-ADC_HIGH = 65535
-
 
 
 # Create a socket
@@ -39,6 +39,27 @@ ADC_REF = photoresistor.reference_voltage
 
 def adc_to_voltage(adc_value):
     return ADC_REF * (float(adc_value)/float(ADC_HIGH))
+
+tx_pin = board.GP0  # can be any pin marked TX (see pin map diagram)
+rx_pin = board.GP1  # can be any pin marked RX (see pin map diagram)
+uart = busio.UART(tx_pin, rx_pin, baudrate=9600, timeout=10)
+
+gps = adafruit_gps.GPS(uart, debug = False)
+
+
+# Turn on just minimum info (RMC only, location):
+gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+gps.send_command(b"PMTK220,1000")  # 1Hz update rate
+
+#* Other options for info
+#? Turn on the basic GGA and RMC info (what you typically want)
+# gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+#? Turn on the basic GGA and RMC info + VTG for speed in km/h
+# gps.send_command(b"PMTK314,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+#? Turn off everything:
+# gps.send_command(b'PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+#? Turn on everything (not all of it is parsed!)
+# gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
 
 # Data sending loop
 while True:
