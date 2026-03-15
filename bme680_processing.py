e46f1106bf82c53f286630d62e6a886104d8a48f
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-03-15 16:28:25 -0400

    add data from bme680 measurements

---
+++ bme680_processing.py
@@ -0,0 +1,8 @@
+resistances = []
+
+with open("gas_data.csv", 'r') as f:
+    for line in f.readlines()[1:]:  # skip the header
+        resistance = (line.split(",")[1].replace("\n", ''))
+        resistances.append(int(resistance))
+
+print(f"Average Resistance: {sum(resistances)/len(resistances)} Ohms")
