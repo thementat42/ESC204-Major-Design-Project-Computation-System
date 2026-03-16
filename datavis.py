1b5e32aafc0b85fa511373ec16953a39d460394d
Mahtasim <surhid2006@gmail.com>
2026-03-15 20:24:35 -0400

    Made in progress linked list and dictionary representations of the system

--- datavis.py
+++ datavis.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 import pandas as pd
+import json
 
 # ___________________________________________________________________
 # TEST JSONS
@@ -27,27 +28,27 @@ module1_data = """
 }
 
 """
-# module2_data = """
-# {
+module2_data = """
+{
 
-# "id": 2,
+"id": 2,
 
-# "temperature": 30,
+"temperature": 30,
 
-# "humidity": 45,
+"humidity": 45,
 
-# "pressure": 80,
+"pressure": 80,
 
-# "gas": 90,
+"gas": 90,
 
-# "light": 0.1100,
+"light": 0.1100,
 
-# "latitude": 20.44,
+"latitude": 20.44,
 
-# "longitude": -30.35
-# }
+"longitude": -30.35
+}
 
-# """
+"""
 
 # module3_data = """
 # {
@@ -71,110 +72,160 @@ module1_data = """
 
 # """
 
-# ___________________________________________________________________
-# MAIN CODE
+modules = [module1_data, module2_data]
 
+# ___________________________________________________________________
 # Tasks:
+    # Make module and system classes - DONE
     # Make marker display (updates in real time)
     # Make markers temperature dependent
     # Make pygame display to search up specific modules
+    # Make a display function to runt the whole thing
 
+# ___________________________________________________________________
+# DICTIONARY REPRESENTATION
+
+def module_list(modules):
+    '''Creates a list of modules in the form of dictionaries'''
+
+    mod_list = []
+    for mod in modules:
+        set = json.loads(mod)
+        mod_list.append(set)
+    return mod_list
+
+def values_list(modlist):
+    temps = [] #list of temperatures to plot
+
+    for mod in modlist:
+
+
+# ___________________________________________________________________
+# LINKED LIST REPRESENTATION
 # Define a module
-class module:
-    def __init__(self, id, temperature, humidity, pressure, gas, light, latitude, longitude):
-        self.id = id
-        self.temperature = temperature
-        self.humidity = humidity
-        self.pressure = pressure
-        self.gas = gas
-        self.light = light
-        self.coords = (latitude, longitude)
-        self.next = None # Allows modules to be iterated through via a linked list
+# class module:
+#     def __init__(self, id, temperature, humidity, pressure, gas, light, latitude, longitude):
+#         self.id = id
+#         self.temperature = temperature
+#         self.humidity = humidity
+#         self.pressure = pressure
+#         self.gas = gas
+#         self.light = light
+#         self.coords = (latitude, longitude)
+#         self.next = None # Allows modules to be iterated through via a linked list
 
-# Define a system of modules        
-class system:
-    def __init__(self):
-        self.start = None
+# # Define a system of modules        
+# class system:
+#     def __init__(self):
+#         self.start = None
     
-    def size(self):
-        '''Returns the number of modules in the system'''
+#     def size(self):
+#         '''Returns the number of modules in the system'''
 
-        if self.start == None:
-            return 0
+#         if self.start == None:
+#             return 0
 
-        num = 0 # The number of modules
-        cur = self.start
-        while cur:
-            while cur.next != None:
-                num += 1
-                cur = cur.next
+#         num = 0 # The number of modules
+#         cur = self.start
+#         while cur:
+#             while cur.next != None:
+#                 num += 1
+#                 cur = cur.next
         
-        return num
+#         return num
 
-    def values(self, id):
-        '''Returns the values of a given module of the specified index'''
+#     def values(self, id):
+#         '''Returns the values of a given module of the specified index'''
 
-        if self.start == None:
-            return "System Empty"
+#         if self.start == None:
+#             return "System Empty"
         
-        # Check the size of the system to make sure id not out of range
-        num = 0 # The number of modules
-        cur = self.start
-        while cur:
-            while cur.next != None:
-                num += 1
-                cur = cur.next
+#         # Check the size of the system to make sure id not out of range
+#         num = 0 # The number of modules
+#         cur = self.start
+#         while cur:
+#             while cur.next != None:
+#                 num += 1
+#                 cur = cur.next
         
-        if id > num:
-            return "Invalid Index"
+#         if id > num:
+#             return "Invalid Index"
         
-        # Find the module at the specified index
-        cur = self.start
-        for i in range(id - 1):
-            cur = cur.next
+#         # Find the module at the specified index
+#         cur = self.start
+#         for i in range(id - 1):
+#             cur = cur.next
 
-        return cur.coords, cur.temperature, cur.humidity, cur.pressure, cur.gas, cur.light
+#         return cur.coords, cur.temperature, cur.humidity, cur.pressure, cur.gas, cur.light
     
-    def add_module(self, temperature, humidity, pressure, gas, light, latitude, longitude):
-        '''Adds a new module'''
+#     def add_module(self, new_module):
+#         '''Adds a new module'''
 
-        # Check the size of the system 
-        num = 0 # The number of modules
-        cur = self.start
-        while cur:
-            while cur.next != None:
-                num += 1
-                cur = cur.next
+#         # Check the size of the system 
+#         num = 0 # The number of modules
+#         cur = self.start
+#         while cur:
+#             while cur.next != None:
+#                 num += 1
+#                 cur = cur.next
         
-        new_module = module((num + 1), temperature, humidity, pressure, gas, light, latitude, longitude)
+#         # new_module = module((num + 1), temperature, humidity, pressure, gas, light, latitude, longitude)
+#         if self.start == None:
+#             self.start = new_module
         
-        if self.start == None:
-            self.start = new_module
-        
-        # Create a new module to add to the system
-        cur = self.start
-        for i in range(num - 1):
-            cur = cur.next
-        cur.next = new_module
+#         # Create a new module to add to the system
+#         cur = self.start
+#         for i in range(num - 1):
+#             cur = cur.next
+#         cur.next = new_module
     
-    def update(self, id, temperature, humidity, pressure, gas, light):
-        '''Updates module values for display'''
-        if id == 1:
-            self.start.temperature = temperature
-            self.start.humidity = humidity
-            self.start.pressure = pressure
-            self.start.gas = gas
-            self.start.light = light
+#     def update(self, id, temperature, humidity, pressure, gas, light):
+#         '''Updates module values for display'''
 
-        else:
-            cur = self.start
-            for i in range(id):
-                cur = cur.next
-            cur.temperature = temperature
-            cur.humidity = humidity
-            cur.pressure = pressure
-            cur.gas = gas
-            cur.light = light
+#         if id == 1:
+#             self.start.temperature = temperature
+#             self.start.humidity = humidity
+#             self.start.pressure = pressure
+#             self.start.gas = gas
+#             self.start.light = light
+
+#         else:
+#             cur = self.start
+#             for i in range(id):
+#                 cur = cur.next
+#             cur.temperature = temperature
+#             cur.humidity = humidity
+#             cur.pressure = pressure
+#             cur.gas = gas
+#             cur.light = light
+
+# # ___________________________________________________________________ 
+# # FUCNTIONS FOR LINKED LIST REPRESENTATION
+
+# def extract_values(string):
+#     '''Exctract data from a json string and create a module out of it'''
+#     set = json.loads(string)
+
+#     id = set["id"]
+#     temperature = set["temperature"]
+#     humidity = set["humidity"]
+#     pressure = set["pressure"]
+#     gas = set["gas"]
+#     light = set["light"]
+#     latitude = set["latitude"]
+#     longitude = set["longitude"]
+
+#     mod = module(id, temperature, humidity, pressure, gas, light, latitude, longitude)
+
+#     return mod
+
+# def create_system(list):
+#     '''Takes in a list of json strings and makes a linked list (system) of modules out of them'''
+#     sys = system()
+#     for string in list:
+#         new_mod = extract_values(string)
+#         sys.add_module(new_mod)
+#     return sys
 
 # ___________________________________________________________________
 # RUN
