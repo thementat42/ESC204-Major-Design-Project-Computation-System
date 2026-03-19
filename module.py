8ce2b0fb8d807aa3ebafe8cc3cdd82303810940e
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-03-19 13:10:36 -0400

    add lux proxy

--- module.py
+++ module.py
@@ -20,53 +20,55 @@ MODULE_ID = 1 # Make sure to change this for every pico
 ADC_HIGH = 65535
 BME_GAS_MIN = 10167
 BME_GAS_THRESHOLD = 63930.51612903226  # High resistance = fresh air
+PHOTORESISTOR_MIN = 0.01919811157142857  # volts (low voltage = bright)
+PHOTORESISTOR_MAX = 3.115979861111111  # volts
 
 # Connect Pico to wi-fi
-wifi.radio.connect(WIFI_SSID, WIFI_PASSWORD)
-print(f"Successfully Connected to {WIFI_SSID}")
+#wifi.radio.connect(WIFI_SSID, WIFI_PASSWORD)
+#print(f"Successfully Connected to {WIFI_SSID}")
 
 time.sleep(5)
 
 # Create a socket
-pool = socketpool.SocketPool(wifi.radio)
+#pool = socketpool.SocketPool(wifi.radio)
 
-print("Testing DNS resolution...")
-try:
+#print("Testing DNS resolution...")
+#try:
     # Test 1: Can we resolve a basic website?
-    google_ip = pool.getaddrinfo("google.com", 80)[0][4][0]
-    print(f"Basic DNS works (Google IP: {google_ip})")
+#    google_ip = pool.getaddrinfo("google.com", 80)[0][4][0]
+#    print(f"Basic DNS works (Google IP: {google_ip})")
     
     # Test 2: Can we resolve HiveMQ broker?
-    hivemq_ip = pool.getaddrinfo(HIVEMQ_HOST, 8883)[0][4][0]
-    print(f"HiveMQ DNS works (HiveMQ IP: {hivemq_ip})")
-except Exception as e:
-    print(f"DNS FAILED! Error: {e}")
+#    hivemq_ip = pool.getaddrinfo(HIVEMQ_HOST, 8883)[0][4][0]
+#    print(f"HiveMQ DNS works (HiveMQ IP: {hivemq_ip})")
+#except Exception as e:
+#    print(f"DNS FAILED! Error: {e}")
 
-ssl_context = ssl.create_default_context()
+#ssl_context = ssl.create_default_context()
 
-ntp = adafruit_ntp.NTP(pool, tz_offset=0)
-rtc.RTC().datetime = ntp.datetime
-print("RTC time:", rtc.RTC().datetime)
+#ntp = adafruit_ntp.NTP(pool, tz_offset=0)
+#rtc.RTC().datetime = ntp.datetime
+#print("RTC time:", rtc.RTC().datetime)
 
-mqtt_logger = logging.getLogger("mqtt")
+#mqtt_logger = logging.getLogger("mqtt")
 #mqtt_logger.setLevel(logging.DEBUG)
 
 # Create MQTT client with necessary information on standard port
-mqtt_client = MQTT.MQTT(
-    broker=HIVEMQ_HOST,
-    port=8883,
-    username = HIVEMQ_USERNAME,
-    password = HIVEMQ_PASSWORD,
-    socket_pool = pool,
-    ssl_context=ssl_context,
-    client_id = str(MODULE_ID),
-    is_ssl=True,
-    keep_alive = 60
-    )
-mqtt_client.logger = mqtt_logger
+#mqtt_client = MQTT.MQTT(
+#    broker=HIVEMQ_HOST,
+#    port=8883,
+#    username = HIVEMQ_USERNAME,
+#    password = HIVEMQ_PASSWORD,
+#    socket_pool = pool,
+#    ssl_context=ssl_context,
+#    client_id = str(MODULE_ID),
+#    is_ssl=True,
+#    keep_alive = 60
+#    )
+# mqtt_client.logger = mqtt_logger
 
-print("Connecting to HiveMQ Cloud with debug logs ON...")
-mqtt_client.connect()
+print("Connecting to HiveMQ Cloud...")
+#mqtt_client.connect()
 print("Connected using MQTT")
 
 # Create i2c protocol for the pico to be able to speak to the bme680
@@ -81,14 +83,18 @@ photoresistor_pin = board.GP26_A0
 photoresistor = analogio.AnalogIn(photoresistor_pin)
 ADC_REF = photoresistor.reference_voltage
 
-def adc_to_voltage(adc_value):
-    return ADC_REF * (float(adc_value)/float(ADC_HIGH))
-
 def get_air_quality_proxy(gas_resistance):
     # ideally, we would use the Bosch BSEC Library
     # however that is only compatible with arduinos and is closed source
     # this is a proxy
     return (gas_resistance-BME_GAS_MIN)/(BME_GAS_THRESHOLD-BME_GAS_MIN)
+
+def get_lux_proxy(adc_value):
+    voltage = ADC_REF * (float(adc_value)/float(ADC_HIGH))
+    v = max(min(voltage, PHOTORESISTOR_MAX), PHOTORESISTOR_MIN)
+
+    # Inverted so smaller = darker
+    return (PHOTORESISTOR_MAX-v) / (PHOTORESISTOR_MAX - PHOTORESISTOR_MIN)
 
 def get_gps_data(gps: adafruit_gps.GPS):
     if not gps.has_fix:
@@ -120,7 +126,8 @@ gps.send_command(b"PMTK220,1000")  # 1Hz update rate
 # Data sending loop
 while True:
     try:
-        mqtt_client.loop()
+        pass
+        #mqtt_client.loop()
     except Exception as e:
         print("MQTT Loop Error:", e)
     gps.update()
@@ -134,13 +141,13 @@ while True:
         HUMIDITY: bme.relative_humidity, 
         PRESSURE: bme.pressure, 
         GAS: get_air_quality_proxy(bme.gas), 
-        LIGHT: adc_to_voltage(photoresistor.value), 
+        LIGHT: get_lux_proxy(photoresistor.value), 
         LATITUDE: latitude,
         LONGITUDE: longitude
         })
     
     # Send json string to laptop under station/m#/data
-    mqtt_client.publish("station/" + str(MODULE_ID) + '/data', string)
+    #mqtt_client.publish("station/" + str(MODULE_ID) + '/data', string)
     print("Sent:", string)
     
     # Wait 1 second to send at 1Hz
